6a76b8958374ec9591092f7a5e0d33e429497fbe
wonderful <wonderful27x@126.com>
2020-05-19 21:18:18 +0800

    as git 提交测试

--- app/src/main/java/com/example/cameratest/MainActivity.java
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -17,6 +17,8 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
+    //TODO Git test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
